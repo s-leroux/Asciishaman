8defb65a3507e42928fc627177fdec092d0c9b5f
Sylvain Leroux <sylvain@chicoree.fr>
2021-01-25 19:16:55 +0100

    Tokenize unordered lists (level 1) and block titles

--- test/tokenizer.js
+++ test/tokenizer.js
@@ -11,28 +11,45 @@ function fixture(fName) {
 
 const tokenizer = require("../lib/tokenizer.js");
 
-describe("module", function() {
-    it("should tokenize section titles", function() {
-        const result = [];
-        const callbacks = {
-            endOfLine(data) {
-                result.push(["endOfLine", data]);
-            },
-            text(data) {
-                result.push(["text", data]);
-            },
-            sectionTitle1(data) {
-                result.push(["sectionTitle1", data]);
-            },
-            sectionTitle2(data) {
-                result.push(["sectionTitle2", data]);
-            },
-            sectionTitle3(data) {
-                result.push(["sectionTitle3", data]);
-            },
-        };
+function tkTest(fName, expected) {
+    const result = [];
+    const callbacks = {
+        endOfLine(data) {
+            result.push(["endOfLine", data]);
+        },
+        text(data) {
+            result.push(["text", data]);
+        },
+        unorderedList1(data) {
+            result.push(["unorderedList1", data]);
+        },
+        sectionTitle1(data) {
+            result.push(["sectionTitle1", data]);
+        },
+        sectionTitle2(data) {
+            result.push(["sectionTitle2", data]);
+        },
+        sectionTitle3(data) {
+            result.push(["sectionTitle3", data]);
+        },
+        title(data) {
+            result.push(["title", data]);
+        },
+    };
 
-        const expected = [
+    return tokenizer.Tokenizer(
+        fs.createReadStream(fixture(fName)),
+        callbacks
+    ).then(() => {
+        assert.deepEqual(result, expected);
+    });
+}
+
+describe("tokenizer", function() {
+    this.timeout(10);
+
+    it("should tokenize section titles", function() {
+        return tkTest("sections_1.adoc", [
           [ "sectionTitle2", "==" ],
           [ "text", " First Section" ],
           [ "endOfLine", "\n" ],
@@ -53,14 +70,23 @@ describe("module", function() {
           [ "endOfLine", "\n" ],
           [ "text", "Content of second section" ],
           [ "endOfLine", "\n" ],
-        ];
+        ]);
+    });
 
-        return tokenizer.Tokenizer(
-          fs.createReadStream(fixture("sections_1.adoc")),
-          callbacks
-        ).then(() => {
-          assert.deepEqual(result, expected);
-        });
+    it("should tokenize unordered lists with title", function() {
+        return tkTest("lists_1.adoc", [
+          [ "title", ".Kizmet's Favorite Authors" ],
+          [ "endOfLine", "\n" ],
+          [ "unorderedList1", "*" ],
+          [ "text", " Edgar Allan Poe" ],
+          [ "endOfLine", "\n" ],
+          [ "unorderedList1", "*" ],
+          [ "text", " Sheri S. Tepper" ],
+          [ "endOfLine", "\n" ],
+          [ "unorderedList1", "*" ],
+          [ "text", " Bill Bryson" ],
+          [ "endOfLine", "\n" ],
+        ]);
     });
 });
 
