bb64d6588988f008f7ced111da994085f5e6d2ac
Sylvain Leroux <sylvain@chicoree.fr>
2021-01-27 11:23:45 +0100

    Draft of the parser

---
+++ test/parser.js
@@ -0,0 +1,34 @@
+const debug = require("debug")("asciishaman:tests-parser");
+const chai = require('chai');
+const assert = chai.assert;
+
+
+const fs = require('fs');
+const path = require('path');
+
+function fixture(fName) { // XXX move me to test/utils.js
+  return path.join(__dirname, "fixtures", fName);
+} 
+
+const parser = require("../lib/parser.js");
+
+function tkTest(fName, expected) {
+    return parser.Parser(
+        fs.createReadStream(fixture(fName), { highWaterMark: 16 })
+    ).then((result) => {
+        assert.deepEqual(result, expected);
+    });
+}
+
+describe("tokenizer", function() {
+    this.timeout(10);
+
+    it("should parse paragraphs", function() {
+        return tkTest("paragraph_1.adoc")
+          .then(ast => {
+            debug(ast);
+          });
+    });
+
+});
+
