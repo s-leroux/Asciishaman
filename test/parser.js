1e2fd770c207b3b7f165ef9f387f01c212143875
Sylvain Leroux <sylvain@chicoree.fr>
2021-01-27 23:01:58 +0100

    Report errors through the diagnostic object

--- test/parser.js
+++ test/parser.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 function fixture(fName) { // XXX move me to test/utils.js
   return path.join(__dirname, "fixtures", fName);
-} 
+}
 
 const parser = require("../lib/parser");
 const { Diagnostic } = require("../lib/diagnostic");
@@ -30,6 +30,7 @@ function tkTest(fName, expected) {
         // assert.deepEqual(result, expected);
     })
     .then(() => {
+        console.log(diagnostic);
         assert.isTrue(diagnostic.success());
         assert.equal(output.toString(), expected);
     });
@@ -39,8 +40,16 @@ describe("parser", function() {
     this.timeout(10);
 
     it("should parse paragraphs", function() {
-        return tkTest("paragraph_1.adoc", 
+        return tkTest("paragraph_1.adoc",
                       "<body><p><span>Paragraphs don't require any special markup in AsciiDoc. A paragraph is just one or more lines of consecutive text.</span></p><p><span>To begin a new paragraph, separate it by at least one blank line from the previous paragraph or block.</span></p></body>\n")
+          .then(ast => {
+            debug(ast);
+          });
+    });
+
+    it("should parse sections", function() {
+        return tkTest("sections_1.adoc",
+                      "<body></body>\n")
           .then(ast => {
             debug(ast);
           });
