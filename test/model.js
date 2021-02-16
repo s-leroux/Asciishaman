2d2073861227fc1039d22b54e668839aa3cf793b
Sylvain Leroux <sylvain@chicoree.fr>
2021-02-16 15:03:51 +0100

    Partial implementation of the styler

--- test/model.js
+++ test/model.js
@@ -3,21 +3,71 @@ const chai = require('chai');
 const assert = chai.assert;
 
 
-const model = require("../lib/model.js");
+const dom = require("../lib/model.js");
 
 
-describe("model", function() {
+describe("utils", function() {
+
+  describe("string parser", function() {
+      this.timeout(10);
+
+      it("should keep plaintext intect", function() {
+          const style = dom._breakString("Hello");
+
+          assert.deepEqual(style, [
+            0x00, "Hello",
+            0x00,
+          ]);
+      });
+
+      it("should parse strong", function() {
+          const style = dom._breakString("Hello *world* !");
+          assert.deepEqual(style, [
+            0x00, "Hello ",
+            0x01, "world",
+            0x00, " !",
+            0x00,
+          ]);
+      });
+
+      it("should parse italic", function() {
+          const style = dom._breakString("Hello _world_ !");
+          assert.deepEqual(style, [
+            0x00, "Hello ",
+            0x02, "world",
+            0x00, " !",
+            0x00,
+          ]);
+      });
+
+      it("should parse stong+italic", function() {
+          const style = dom._breakString("This *is a _strong_ text* !");
+          assert.deepEqual(style, [
+            0x00, "This ",
+            0x01, "is a ",
+            0x03, "strong",
+            0x01, " text",
+            0x00, " !",
+            0x00
+          ]);
+      });
+
+  });
+
+});
+
+describe("dom", function() {
     this.timeout(10);
 
     it("should create document", function() {
-        const document = new model.Document();
+        const document = new dom.Document();
 
         assert.isNotNull(document);
         assert.deepEqual(document.children, []);
     });
 
     it("should create paragraph", function() {
-        const paragraph = new model.Paragraph();
+        const paragraph = new dom.Paragraph();
 
         assert.isNotNull(paragraph);
     });
