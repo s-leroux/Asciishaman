ce269f0a10ea7241cc8c1608db32d4a829150fc7
Sylvain Leroux <sylvain@chicoree.fr>
2021-02-25 19:33:17 +0100

    Add Monospace and Superscript to the inline parser

--- test/inline-parser.js
+++ test/inline-parser.js
@@ -19,44 +19,66 @@ describe("string parser", function() {
 
     it("should keep plaintext intect", function() {
         const style = ip.parseText("Hello");
-        
+
         return dump(style).then((html) => assert.equal(html, "Hello"));
     });
 
     it("should parse strong", function() {
         const style = ip.parseText("Hello *world* !");
-        
+
         return dump(style).then((html) => assert.equal(html, "Hello <strong>world</strong> !"));
     });
 
     it("should parse multiple strong", function() {
         const style = ip.parseText("*Hello* *world* !");
-        
+
         return dump(style).then((html) => assert.equal(html, "<strong>Hello</strong> <strong>world</strong> !"));
     });
 
     it("should parse italic", function() {
         const style = ip.parseText("Hello _world_ !");
-        
+
         return dump(style).then((html) => assert.equal(html, "Hello <em>world</em> !"));
     });
 
     it("should parse stong+italic", function() {
         const style = ip.parseText("This *is a _strong_ text* !");
-        
+
         return dump(style).then((html) => assert.equal(html, "This <strong>is a <em>strong</em> text</strong> !"));
     });
 
     it("should parse style markers at the start of a line", function() {
         const style = ip.parseText("*world* !");
-        
+
         return dump(style).then((html) => assert.equal(html, "<strong>world</strong> !"));
     });
 
     it("should parse style markers at both the start and the end of a line", function() {
         const style = ip.parseText("*world*");
-        
+
         return dump(style).then((html) => assert.equal(html, "<strong>world</strong>"));
+    });
+
+    it("should parse superscript", function() {
+        const style = ip.parseText("Hello ^world^ !");
+
+        return dump(style).then((html) => assert.equal(html, "Hello <sup>world</sup> !"));
+    });
+
+    it("should parse monospace", function() {
+        const style = ip.parseText("Hello `world` !");
+
+        return dump(style).then((html) => assert.equal(html, "Hello <tt>world</tt> !"));
+    });
+
+    describe("mixed content", function() {
+
+      it("should parse monospace", function() {
+          const style = ip.parseText("This is *a _deep *nested* ^quoted^ text_!!!");
+
+          return dump(style).then((html) => assert.equal(html, "This is *a <em>deep <strong>nested</strong> <sup>quoted</sup> text</em>!!!"));
+      });
+
     });
 
 });
