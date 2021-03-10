dbfb8a7ffc91d12f4c1fca1f26f0899e96767f06
Sylvain Leroux <sylvain@chicoree.fr>
2021-03-11 00:12:19 +0100

    Pass content as arg to the constructor
    in a pure bottom-up parsing scheme

--- test/model.js
+++ test/model.js
@@ -6,18 +6,18 @@ const assert = chai.assert;
 
 const dom = require("../lib/model.js");
 
-describe("dom", function() {
+describe("model", function() {
   this.timeout(10);
 
   it("should create document", function() {
-    const document = new dom.Document();
+    const document = new dom.Document([]);
 
     assert.isNotNull(document);
     assert.deepEqual(document.children, []);
   });
 
   it("should create paragraph", function() {
-    const paragraph = new dom.Paragraph();
+    const paragraph = new dom.Paragraph([]);
 
     assert.isNotNull(paragraph);
   });
